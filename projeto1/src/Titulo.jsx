190336ebac089bc754aae94253089f0ba212116e
Abraão Borges <abraao10clear@gmail.com>
2024-01-14 12:47:13 -0300

    Last update

--- projeto1/src/Titulo.jsx
+++ projeto1/src/Titulo.jsx
@@ -1,7 +1,11 @@
 function Titulo() {
+  let nome = "Abraão Borges";
+  const urlImg =
+    "https://www.jsweet.org/wp-content/uploads/2016/04/react-logo.png";
   return (
     <div>
-      <h1> Oi, eu sou um Título!</h1>
+      <h1> Oi, eu sou o {nome} !</h1>
+      <img width= {100} src={urlImg} alt="" />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
         quaerat delectus consectetur libero minima quis officiis quos. Impedit
